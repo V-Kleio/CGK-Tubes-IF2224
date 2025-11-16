b1ca8cf640de4d7c50c7ea863625055e269e31aa
drianto <andri070805@gmail.com>
2025-11-16 17:37:31 +0700

    test: add new test case when syntax error

---
+++ test/milestone-2/input-6.pas
@@ -0,0 +1,14 @@
+program UTS;
+
+variabel
+    pekan, i: integer;
+
+mulai
+    pekan := 8;
+
+    jika pekan = 8 maka
+        writeln('Semangat UTS')
+    selain_itu
+        writeln('Nugas moal?');
+    untuk i := 3;
+selesai.
